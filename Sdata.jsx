9c9c52d4998bed7f56854f0dd29aa9b22d8554c3
2002shreyadas <86796095+2002shreyadas@users.noreply.github.com>
2024-02-04 18:18:08 +0530

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata = [
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname: 'DARK',
+        link : "https://www.netflix.com/in/title/80100172",
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp7418550.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname : 'Extra curricular',
+        link : "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp4431942.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname : 'Stranger Things',
+        link : "https://www.netflix.com/in/title/80057281",
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp6886019.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname : 'Warrior Nun',
+        link : "https://www.netflix.com/in/title/80242724",
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp12070512.jpg" ,
+        title:"A Netflix Original Series" ,
+        sname :'Peaky Blinders',
+        link :"https://www.netflix.com/in/title/80002479",
+    },
+]
+export default Sdata;
